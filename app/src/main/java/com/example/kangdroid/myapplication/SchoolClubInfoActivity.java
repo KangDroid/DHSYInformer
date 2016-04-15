17e49927b087a32bf6898f92be76dc022f31aa25
KangDroid <hyunwoo286@gmail.com>
2016-04-16 01:29:33 +0900

    Add: Add School Club Information Activity
    EMPTY FOR NOW!!!

---
+++ app/src/main/java/com/example/kangdroid/myapplication/SchoolClubInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.kangdroid.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by KangDroid on 2016. 4. 3..
+ */
+public class SchoolClubInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.school_club_info);
+    }
+}

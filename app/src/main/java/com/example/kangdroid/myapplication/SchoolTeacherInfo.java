3ad5f3664d4229d223c465d82eaac6eeb9b04c99
KangDroid <hyunwoo286@gmail.com>
2016-04-15 21:27:55 +0900

    Initial commit
    First commit, DHSYInformer is now OpenSource!
    For now its currently WIP.
    
    Last Tested: On Shamu(Nexus6) at 20160408

---
+++ app/src/main/java/com/example/kangdroid/myapplication/SchoolTeacherInfo.java
@@ -0,0 +1,15 @@
+package com.example.kangdroid.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KangDroid on 2016. 4. 8..
+ */
+public class SchoolTeacherInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.school_teacher_info);
+    }
+}

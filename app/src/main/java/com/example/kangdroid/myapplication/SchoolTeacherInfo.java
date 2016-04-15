665935620e32e58f7ba60611e94fb34e2517fdd4
KangDroid <hyunwoo286@gmail.com>
2016-04-16 01:11:35 +0900

    Add: Add Teacher's Databases and show it to TableLayout
    Actually it took 2hs to show it to TableLayout.

--- app/src/main/java/com/example/kangdroid/myapplication/SchoolTeacherInfo.java
+++ app/src/main/java/com/example/kangdroid/myapplication/SchoolTeacherInfo.java
@@ -1,15 +1,153 @@
 package com.example.kangdroid.myapplication;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by KangDroid on 2016. 4. 8..
  */
 public class SchoolTeacherInfo extends AppCompatActivity {
+    private Context context;
+	public static final String ROOT_DIR = "/data/data/com.example.kangdroid.myapplication/databases/";
+    public static final String TABLE_OUTLET = "tbloutletdata";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.school_teacher_info);
+        context = this;
+        setDB(context);
+
+        // Reference to TableLayout
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tablelayout);
+        // Add header row
+        TableRow rowHeader = new TableRow(context);
+        rowHeader.setBackgroundColor(Color.parseColor("#c0c0c0"));
+        rowHeader.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+        String[] headerText = {"이름", "직위", "과목", "학년/반"};
+        for (String c : headerText) {
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            tv.setGravity(Gravity.CENTER);
+            tv.setTextSize(18);
+            tv.setPadding(5, 5, 5, 5);
+            tv.setText(c);
+            rowHeader.addView(tv);
+        }
+        tableLayout.addView(rowHeader);
+
+        // Get data from sqlite database and add them to the table
+        // Open the database for reading
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(ROOT_DIR+"outletdb", null, 0);
+        // Start the transaction.
+        db.beginTransaction();
+
+        try {
+            String selectQuery = "SELECT * FROM " + TABLE_OUTLET;
+            Cursor cursor = db.rawQuery(selectQuery, null);
+            if (cursor.getCount() > 0) {
+                while (cursor.moveToNext()) {
+                    // Read columns data
+                    //int outlet_id = cursor.getInt(cursor.getColumnIndex("outlet_id"));
+                    String outlet_name = cursor.getString(cursor.getColumnIndex("outlet_name"));
+                    String outlet_type = cursor.getString(cursor.getColumnIndex("outlet_type"));
+                    String outlet_kdp = cursor.getString(cursor.getColumnIndex("outlet_kdp"));
+                    String outlet_classes = cursor.getString(cursor.getColumnIndex("outlet_classes"));
+
+                    // dara rows
+                    TableRow row = new TableRow(context);
+                    row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
+                    String[] colText = {outlet_name+"", outlet_type, outlet_kdp, outlet_classes};
+                    for (String text : colText) {
+                        TextView tv = new TextView(this);
+                        tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                                TableRow.LayoutParams.WRAP_CONTENT));
+                        tv.setGravity(Gravity.CENTER);
+                        tv.setTextSize(16);
+                        tv.setPadding(5, 5, 5, 5);
+                        tv.setText(text);
+                        row.addView(tv);
+                    }
+                    tableLayout.addView(row);
+
+                }
+
+            }
+            db.setTransactionSuccessful();
+
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+
+        } finally {
+            db.endTransaction();
+            // End the transaction.
+            db.close();
+            // Close database
+        }
+    }
+	
+    public static void setDB(Context ctx) {
+        File folder = new File(ROOT_DIR);
+        if(folder.exists()) {
+            //Update manually by deleting and makings
+            folder.delete();
+            folder.mkdirs();
+        } else {
+            folder.mkdirs();
+        }
+        AssetManager assetManager = ctx.getResources().getAssets(); //ctx가 없으면 assets폴더를 찾지 못한다.
+        File outfile = new File(ROOT_DIR+"outletdb");
+        outfile.delete();
+        InputStream is = null;
+        FileOutputStream fo = null;
+        long filesize = 0;
+        try {
+            is = assetManager.open("outletdb", AssetManager.ACCESS_BUFFER);
+            filesize = is.available();
+            if (outfile.length() <= 0) {
+                byte[] tempdata = new byte[(int) filesize];
+                is.read(tempdata);
+                is.close();
+                outfile.createNewFile();
+                fo = new FileOutputStream(outfile);
+                fo.write(tempdata);
+                fo.close();
+            } else {}
+        } catch (IOException e) {}
+    }
+
+    class ProductDBHelper extends SQLiteOpenHelper {  //새로 생성한 adapter 속성은 SQLiteOpenHelper이다.
+        public ProductDBHelper(Context context) {
+            super(context, "outletdb", null, 1);    // db명과 버전만 정의 한다.
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // TODO Auto-generated method stub
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+// TODO Auto-generated method stub
+        }
     }
 }
